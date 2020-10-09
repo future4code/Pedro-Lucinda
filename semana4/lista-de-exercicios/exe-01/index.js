c18620f6c547729e1f5d1cb77f0197f4fbfa7af2
Your full name <Your_email_address_what_used_to_register Github_account>
2020-10-09 10:51:29 -0300

    Exercicios de interpretacao de codigo

---
+++ semana4/lista-de-exercicios/exe-01/index.js
@@ -0,0 +1,69 @@
+// Exercicios de interpretacao de codigo
+
+/* Exe-01
+
+    A funcao conversorDeMoeda que recebe como parametro valorEmDolar. Primeiro pede para o usuario colocar o valor em dollar. A variavel que guarda o valor ja transforma a string que o usuario colocou em numero.
+
+    A funcao retorna R$ + valor do parametro vezes valor inserido pelo usuario. 
+
+
+    Depois e criado uma nova variavel que guarda o resultado da funcao recebendo como parametro o numero 100. Depois imprime no console
+
+
+    Resultado = 100*cotacao
+
+
+*/
+
+
+
+/* Exe-02
+
+    Funcao que tem como paramentro tipoDeInvestimento e valor. 
+    E criado uma varavel vazia. 
+
+    Depois e feito um switch para quando o tipo de investimento for igual a uma string especifica sera feito um calculo especifico. 
+    Caso nenhum se encaixe aparecera um alert escrito 'Tipo de investimento informado incorreto"
+
+    Retornara o valorAposInvestimento de acordo com o tipo de investimento e valor do investimento.
+
+
+    Criado uma variavel chamada novoMontante que guarda o retorno da funcao investeDinehiro com os paramentros - Acoes e 150
+
+    Criado uma variavel chamada segundoMontante que guarda o retorno da funcao investeDinehiro com os paramentros - Tesouro direto e 200
+
+    E impresso novoMontante e depois segundoMontante
+
+ */
+
+
+ /* Exe-03
+
+    E dado um array chamado numeros
+    depois e criado 2 array vazios 
+
+    E feito um loop sobre o array numeros para verificar se o numero e par, caso ele seja ele e colocado no array1.
+    Caso ele nao seja par ele e colocado no array2
+    
+
+    Depois e impresso no console 'Quantidade total de numeros, tamanho do array numeros.
+
+    Depois imprime no console os dois arrays com pares e impares.
+
+ */
+
+
+
+/*Exe-04
+
+    E dado um array chamado numeros com varios numeros dentro.
+
+    depois e criado 2 variaveis a primeria (numero1) com valor infinity  e (numero2)segunda com 0
+
+    Depois e feito um loop no array numeros e uma condicao. Se numero que percorre o array de numeros for menor que numero1 entao o valor da variavel numero1 sera igual o numero. 
+
+    Depois e feito outra condicao se o numero que percorre o array numeros for maior que a variavel numero2 entao a variavel numero2 recebe o valor do numero. 
+
+    E impresso no console as variaveis numero1 e numero2
+
+*/
