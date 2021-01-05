6523290f96d603f17fe7de12b18be449f6cb8f03
pedro-lucinda <pedrolucinda2020@gmail.com>
2021-01-05 20:40:16 -0300

    TypeScript

---
+++ semana10/introTypescript/src/index.ts
@@ -0,0 +1,19 @@
+console.log("Hello World")
+
+enum LabenuClasses {
+	MELLO = "Mello",
+	TURING = "Turing",
+	JACKSON = "Jackson",
+	DUMONT = "Dumont"
+	}
+	//enum nao muda
+	
+	
+	type LabenuTeacher = {
+	class: LabenuClasses
+	}
+	
+	const amanda: LabenuTeacher = {
+	class: LabenuClasses.JACKSON
+	}
+	
